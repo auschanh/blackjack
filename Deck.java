6cf60f657abff7cdd2522a19855ef73282e64c87
auschanh <auschanh@gmail.com>
2023-10-04 01:53:25 -0400

    Added new constructor to make standard deck of 52 cards

--- Deck.java
+++ Deck.java
@@ -3,9 +3,24 @@ import java.util.ArrayList;
 public class Deck {
     // manages the deck of cards with an arrayList
     private ArrayList<Card> deck;
-    
+    // default constructor with no arguments
     public Deck(){
         deck = new ArrayList<Card>(); // constructor that makes a deck with arrayList of Cards
+    }
+
+    // overloaded constructor that takes a boolean of True
+    // create a standard deck of 52 cards
+    public Deck(boolean makeDeck) {
+        deck = new ArrayList<Card>();
+        if(makeDeck) {
+             // iterate through enum suits
+            for(Suit suit : Suit.values()) {
+                // iterate through ranks
+                for(Rank rank : Rank.values()) {
+                    deck.add(new Card(suit, rank));
+                }
+            }
+        }
     }
     
     // method to populate the deck
--- Main.java
+++ Main.java
@@ -2,16 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Blackjack");
         Game blackjack = new Game();
-        Deck newDeck = new Deck();
-
-        Card aCard = new Card(Suit.CLUB, Rank.ACE);
-        Card bCard = new Card(Suit.DIAMOND, Rank.TWO);
-        Card cCard = new Card(Suit.HEART, Rank.TEN);
-
-        newDeck.addCard(aCard);
-        newDeck.addCard(bCard);
-        newDeck.addCard(cCard);
-
-        System.out.println(newDeck);
+        Deck testDeck = new Deck(true);
+        
+        System.out.println(testDeck);
     }
 }
