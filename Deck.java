d2bd4458cdd50402774e953bb1a2c0f540cc1960
auschanh <auschanh@gmail.com>
2023-10-04 00:41:36 -0400

    created deck logic that contains arrayList of cards

--- Deck.java
+++ Deck.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
 public class Deck {
+    // manages the deck of cards with an arrayList
+    private ArrayList<Card> deck;
     
+    public Deck(){
+        deck = new ArrayList<Card>(); // constructor that makes a deck with arrayList of Cards
+    }
+    
+    // method to populate the deck
+    public void addCard(Card card){
+        deck.add(card);
+    }
+
+    public String toString(){
+        String output = "";
+        
+        for(Card card: deck) {
+            output += card;
+            output += "\n";
+        }
+        return output;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Blackjack");
         Game blackjack = new Game();
-        Card testCard = new Card(Suit.CLUB, Rank.ACE);
-        System.out.println(testCard);
+        Deck newDeck = new Deck();
+
+        Card aCard = new Card(Suit.CLUB, Rank.ACE);
+        Card bCard = new Card(Suit.DIAMOND, Rank.TWO);
+        Card cCard = new Card(Suit.HEART, Rank.TEN);
+
+        newDeck.addCard(aCard);
+        newDeck.addCard(bCard);
+        newDeck.addCard(cCard);
+
+        System.out.println(newDeck);
     }
 }
