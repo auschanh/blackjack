be50f6ae6a2a47593e701e9087e56c3106af8c4d
auschanh <auschanh@gmail.com>
2023-10-03 20:40:07 -0400

    created classes for blackjack

---
+++ Game.java
@@ -0,0 +1,9 @@
+public class Game {
+    // pushes are ties or draws
+    private int wins, losses, pushes;
+
+    // Constructor
+    public Game() {
+        wins = 0; losses = 0; pushes = 0;
+    }
+}
